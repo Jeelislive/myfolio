af61fda5df2124c85a79dfcbd7315c78f828ff02
Jeel Rupareliya <108856045+Jeelislive@users.noreply.github.com>
2025-03-20 10:38:38 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,65 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap"; // Import Col
+import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/JeelResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = //cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-  const [numPages, setNumPages] = useState(null);
+const [width, setWidth] = useState(1200);
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+useEffect(() => {
+setWidth(window.innerWidth);
+}, []);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+return (
+<div>
+<Container fluid className="resume-section">
+<Particle />
+<Row style={{ justifyContent: "center", position: "relative" }}>
+<Button
+variant="primary"
+href={pdf}
+target="_blank"
+style={{ maxWidth: "250px" }}
+>
+<AiOutlineDownload />
+ Download CV
+</Button>
+</Row>
 
-  return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-             Download CV
-          </Button>
-        </Row>
+<Row className="resume">
+      <Document file={pdf} className="d-flex justify-content-center">
+        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+      </Document>
+    </Row>
 
-        <Row className="resume" style={{ overflowX: "auto" }}>
-          <Col md={{ span: 8, offset: 2 }}> {/* Use Col to center content */}
-            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} >
-              {Array.from(new Array(numPages), (el, index) => (
-                <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.7 : 0.6} />
-              ))}
-            </Document>
-          </Col>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-             Download CV
-          </Button>
-        </Row>
-      </Container>
-    </div>
-  );
+    <Row style={{ justifyContent: "center", position: "relative" }}>
+      <Button
+        variant="primary"
+        href={pdf}
+        target="_blank"
+        style={{ maxWidth: "250px" }}
+      >
+        <AiOutlineDownload />
+         Download CV
+      </Button>
+    </Row>
+  </Container>
+</div>
+);
 }
-
-export default ResumeNew;
