67b984e00024ba395da24c77c66334ec432a2317
Jeel Rupareliya <108856045+Jeelislive@users.noreply.github.com>
2025-03-20 10:36:49 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap"; // Import Col
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/JeelResume.pdf";
@@ -36,12 +36,14 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume" style={{ overflowX: "auto" }}> {/* Added overflowX: "auto" for horizontal scrolling if needed */}
-          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.7 : 0.6} />
-            ))}
-          </Document>
+        <Row className="resume" style={{ overflowX: "auto" }}>
+          <Col md={{ span: 8, offset: 2 }}> {/* Use Col to center content */}
+            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.7 : 0.6} />
+              ))}
+            </Document>
+          </Col>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
